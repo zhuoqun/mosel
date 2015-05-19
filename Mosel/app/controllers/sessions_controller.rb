5098f55c63052b229f81a867b19bc256fb486efe
Jack Wang <zhuoqunwang@gmail.com>
2015-05-19 14:31:29 +0200

    Design the error messages

--- Mosel/app/controllers/sessions_controller.rb
+++ Mosel/app/controllers/sessions_controller.rb
@@ -1,4 +1,26 @@
 class SessionsController < ApplicationController
   def create
   end
+
+  def oauth_success
+  end
+
+  def oauth_failure
+    provider = params[:strategy]
+
+    if provider === 'identity'
+      flash[:error] = t('user.signin_failure')
+    else
+      flash[:error] = t('user.oauth.auth_failure')
+    end
+
+    redirect_to users_signin_path
+  end
+
+  def destroy
+    session[:user_id] = nil
+    cookies.delete :email_hash
+    cookies.delete :user_id
+    redirect_to root_url
+  end
 end
--- Mosel/config/initializers/omniauth.rb
+++ Mosel/config/initializers/omniauth.rb
@@ -1,4 +1,7 @@
 OmniAuth.config.logger = Rails.logger
+OmniAuth.config.on_failure = Proc.new { |env|
+  OmniAuth::FailureEndpoint.new(env).redirect_to_failure
+}
 
 Rails.application.config.middleware.use OmniAuth::Builder do
   provider :evernote, ENV["evernote_api_key"], ENV["evernote_api_secret"]
